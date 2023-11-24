c75c758ea715bf364dc222419a15d1e1f6ec1de7
Kamran Moazim <kamrannaseer765@gmail.com>
2023-11-24 20:46:37 +0500

    added new workflow

--- Program.cs
+++ Program.cs
@@ -34,3 +34,21 @@ app.Run("http://0.0.0.0:9090");
 // AWS_REGISTRY_URL
 // AWS_ACCESS_KEY_ID
 // AWS_SECRET_ACCESS_KEY
+
+
+/*
+
+
+      - name: Update ECS Task Definition
+        run: |
+          aws ecs register-task-definition --family ${{ secrets.ECS_FAMILY }} --container-definitions "$(cat ./container-definition.json)"
+
+      - name: Update ECS Service
+        run: |
+          aws ecs update-service --cluster ${{ secrets.ECS_CLUSTER }} --service ${{ secrets.ECS_SERVICE }} --task-definition ${{ secrets.ECS_FAMILY }}:latest
+
+      - name: Log out of Amazon ECR
+        if: always()
+        run: docker logout ${{ steps.login-ecr.outputs.registry }}
+
+*/
